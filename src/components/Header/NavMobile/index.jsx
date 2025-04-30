ba87cf06182737fb46805698d24bc0a7fe4f38d0
Pedro Visani <p.h.f.v@outlook.com>
2025-04-30 16:19:58 -0300

    correcao css adaptacao geral mobile

--- src/components/Header/NavMobile/index.jsx
+++ src/components/Header/NavMobile/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styles from './NavMobile.module.css';
+import styles from '../Header.module.css';
 
 function NavMobile({ showNavMobile, setShowNavMobile }) {
   if (showNavMobile == true) {
