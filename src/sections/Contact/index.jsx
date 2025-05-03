cbef429b6a730ccc68a756acd610c7428360f23b
Pedro Visani <p.h.f.v@outlook.com>
2025-05-03 15:17:50 -0300

    tentativa correcao form

--- src/sections/Contact/index.jsx
+++ src/sections/Contact/index.jsx
@@ -2,9 +2,9 @@ import styles from './Contact.module.css';
 
 function Contact() {
   return (
-    <section id="contato" className="section main-bg">
+    <section id="contact" className="section main-bg">
       <div className={`${styles['contact-container']} flex-center wh-100`}>
-        <form name="contact" className={styles['contact-form']} data-netlify="true">
+        <form name="contact" className={styles['contact-form']} method="POST" data-netlify="true">
           <fieldset className={styles['fields-form']}>
             <legend className={styles['legend-form']}>ENTRE EM CONTATO CONOSCO</legend>
             <label htmlFor="nome">Nome</label>
