a6809eb232863e55f85c2721a6149a4072e587b1
Pedro Visani <p.h.f.v@outlook.com>
2025-05-09 13:50:16 -0300

    modificacao footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,10 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <footer className={styles['footer']}>
-      <p>Copyright © 2020-2025 C.E.F. DA PAZ SERVIÇOS FINANCEIROS</p>
+      <p>
+        Copyright © 2020-2025 C.E.F. DA PAZ SERVIÇOS FINANCEIROS. CNPJ: 38.112.423/0001-76. <br />{' '}
+        Todos os direitos reservados.
+      </p>
     </footer>
   );
 }
