912332a4ada5cdd565bcbbe140e1a2c638522d2a
Pedro Visani <p.h.f.v@outlook.com>
2025-05-09 11:53:19 -0300

    adicao de plugin vite compress

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import compress from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compress({ algorithm: 'brotliCompress' })],
 });
